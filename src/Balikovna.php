5321ffa1fd095a3989192837fbed1e5a2945cbfd
maremic <misak.mares@gmail.com>
2024-12-04 13:21:32 +0100

    logovani response na balikovna balíku

--- src/Balikovna.php
+++ src/Balikovna.php
@@ -218,7 +218,7 @@ class Balikovna
                 ];
                 return $response->success([$protoParcel]);
             } else {
-//                dd($response);
+                Log::info($response);
                 $response->fail(collect($response->data->responseHeader?->resultParcelData[0]?->parcelStateResponse)->implode('responseText', ', '));
             }
 
